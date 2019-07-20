147e3bb8da8991935031f098a9d931d7ebce9b05
Basan <basanpanthin007@gmail.com>
2019-07-20 18:26:57 +0900

    m

---
+++ submit/16C1010014 Ba/q9.rb
@@ -0,0 +1,4 @@
+
+hash = { January: 1, February: 2, March: 3 , April: 4, May: 5, Jun: 6,July:7,August:8,September:9,October:10,Nvember:11,December:12}
+
+hash.each { |key, value| puts "#{key} => #{value}"}
